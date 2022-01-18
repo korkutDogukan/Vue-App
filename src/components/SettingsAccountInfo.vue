de9910f06a26827c119217af85e325354fa73a76
Doğukan Korkut <doguuu1998@hotmail.com>
2022-01-18 14:43:40 +0300

    Sayfalar arası route işlemi sağlandı

--- src/components/SettingsAccountInfo.vue
+++ src/components/SettingsAccountInfo.vue
@@ -8,7 +8,7 @@
         <tr>
           <td>Username</td>
           <td>:</td>
-          <td>{{ store.state.user.userName }}</td>
+          <td>{{ store.getters._getCurrentUser.userName }}</td>
         </tr>
         <tr>
           <td>Password</td>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import store from "../store";
 
 const routes = [
   {
@@ -31,6 +32,21 @@ const routes = [
 const router = createRouter({
   routes,
   history: createWebHashHistory()
+});
+
+router.beforeEach((to, _, next) => {
+  const authRequiredRoutes = ["HomePage", "ListPage", "SettingsPage"];
+  const authNotRequiredRoutes = ["LoginPage", "RegisterPage"];
+  const _isAuthenticated = store.getters._isAuthenticated;
+
+  if (authNotRequiredRoutes.indexOf(to.name) > -1 && _isAuthenticated) next(false);
+
+  if (authRequiredRoutes.indexOf(to.name) > -1) {
+    if (_isAuthenticated) next();
+    else next({ name: "LoginPage" });
+  } else {
+    next();
+  }
 });
 
 export default router;
